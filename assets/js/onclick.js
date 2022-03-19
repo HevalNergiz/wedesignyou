80743e0716c3632ec064be0eae85f2535213d689
HevalNergiz <1hevalnergiz1@gmail.com>
2022-03-19 13:51:08 +0300

    asdadsadasd

--- assets/js/onclick.js
+++ assets/js/onclick.js
@@ -20,3 +20,22 @@ $('#mail').on('click', function(e){
       e.preventDefault();
  window.open("mailto:info@wedesignyou.co", '_blank').focus();
     });
+
+
+
+$('#instagram1').on('click', function(e){
+      e.preventDefault();
+ window.open("https://www.instagram.com/youizmir/", '_blank').focus();
+    });
+
+$('#facebook1').on('click', function(e){
+      e.preventDefault();
+ window.open("https://www.facebook.com/youizmirtr/", '_blank').focus();
+    });
+
+
+
+$('#mail1').on('click', function(e){
+      e.preventDefault();
+ window.open("mailto:info@wedesignyou.co", '_blank').focus();
+    });
